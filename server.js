d6fd09f8f9b8a95a5d1513bd51182718f8b6aaaf
Cristian Andres Marin Durango <camd1996@gmail.com>
2018-12-26 14:53:58 -0500

    Remove # in routes

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
 var app = express()
-app.use(serveStatic(path.resolve(__dirname) + '/dist'))
+var history = require('connect-history-api-fallback')
 var port = process.env.PORT || 5000
+
+app.use(history())
+app.use(serveStatic(path.resolve(__dirname) + '/dist'))
+
 app.listen(port)
 console.log('server started ' + port)
